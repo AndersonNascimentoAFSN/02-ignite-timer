ae0f9aaa234f41345c448f732230794d68815e06
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-07-09 16:53:07 -0300

    add font-family and colors in the default theme

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -21,7 +21,7 @@ export const ButtonContainer = styled.button<IButtonContainerProps>`
   margin: 8px;
   cursor: pointer;
 
-  background: ${(props) => props.theme.primary};
+  background: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme.white};
 
   /* ${(props) => css`background-color: ${buttonVariants[props.variant]}`} */
