a8c285dd93dfe613ac391316bdb53f499a816e2b
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-07-09 17:44:30 -0300

    add default layout

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from '../layouts/DefaultLayout'
 import { History, Home } from '../pages'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
