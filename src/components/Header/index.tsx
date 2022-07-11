f8e3c51d59be72f538c81af93b2ba7a5ca895228
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-07-11 13:07:41 -0300

    add header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,30 @@
+import { NavLink } from 'react-router-dom'
+import { Scroll, Timer } from 'phosphor-react'
+import Tippy from '@tippyjs/react'
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { HeaderContainer } from './styles'
+import 'tippy.js/dist/tippy.css'
+
 export function Header() {
-  return <div>Header</div>
+  return (
+    <HeaderContainer>
+      <img
+        src={logoIgnite}
+        alt="Logo do ignite, com o formato de dois triângulos
+          de tamanhos diferentes e cor verde"
+      />
+      <nav>
+        <Tippy content="Timer" theme="tomato">
+          <NavLink to="/">
+            <Timer />
+          </NavLink>
+        </Tippy>
+        <Tippy content="Histórico">
+          <NavLink to="/history">
+            <Scroll />
+          </NavLink>
+        </Tippy>
+      </nav>
+    </HeaderContainer>
+  )
 }
